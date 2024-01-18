0e213f506a86230999473441953d329ffc26c4e2
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-01-18 09:37:41 -0600

    initial commit

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class test {
+    public static void main(String args[]) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(7);
+        }
+        System.out.println(list);
+    }
+}
