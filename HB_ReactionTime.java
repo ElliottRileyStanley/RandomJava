25411db5b9ad649f7e3d52b53488547955849ce4
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-01-19 10:14:18 -0600

    a

---
+++ HB_ReactionTime.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.event.InputEvent;
+
+
+public class HB_ReactionTime {
+    public static void main(String args[]) {
+        try {
+            Robot robot = new Robot();
+            while (true) {
+                PointerInfo a = MouseInfo.getPointerInfo();
+                System.out.println();
+                if (robot.getPixelColor((int) a.getLocation().getX(), (int) a.getLocation().getY()).getGreen() > 200) {
+                    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                    break;
+                }
+            }
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- test.java
+++ test.java
@@ -1,23 +1,12 @@
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.InputEvent;
-
 
 public class test {
-    public static void main(String args[]) {
-        try {
-            Robot robot = new Robot();
-            while (true) {
-                PointerInfo a = MouseInfo.getPointerInfo();
-                System.out.println();
-                if (robot.getPixelColor((int) a.getLocation().getX(), (int) a.getLocation().getY()).getGreen() > 200) {
-                    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-                    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-                    break;
-                }
-            }
-        } catch (AWTException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+        JFrame window = new JFrame("");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(new Color(255, 255, 0));
+        window.setVisible(true);
 
     }
 }
