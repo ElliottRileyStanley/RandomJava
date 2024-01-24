5a2c101ca33965e649d44208dbfb869cdbabed83
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-01-23 18:54:41 -0600

    color

--- Physics/test.java
+++ Physics/test.java
@@ -9,59 +9,58 @@ class ball {
     double y;
     double xspeed;
     double yspeed;
-    ball(double x_value, double y_value, double xspeed_value, double yspeed_value) {
-        x = x_value;
-        y = y_value;
-        xspeed = xspeed_value;
-        yspeed = yspeed_value;
-
+    int red;
+    int green;
+    int blue;
+    ball(double x, double y, double xspeed, double yspeed, int red, int green, int blue) {
+        this.x = x;
+        this.y = y;
+        this.xspeed = xspeed;
+        this.yspeed = yspeed;
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
     }
 }
 
 public class test {
     public static void main(String[] args) {
-        /*Initialize window */
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setUndecorated(true);
         window.setVisible(true);
         window.setSize(540, 960);
 
-        /*Initilize graphics */
         Graphics g = window.getGraphics();
 
-        /*Initilize objects */
         ArrayList<ball> balls = new ArrayList<ball>();
-        balls.add(new ball(150, 150, (Math.random()-0.5)*5, (Math.random()-0.5)*5));
+        balls.add(new ball(150.0, 150.0, 1.0, 1.0, 255, 150, 0));
+        balls.add(new ball(150.0, 150.0, 1.0, -1.0, 255, 0, 150));
         System.out.println(balls.get(0).y);
 
         while (true) {
-            try {/*Pause so that it can update (framerate) */
+            try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
             }
+
             g.setColor(Color.white);
             g.fillRect(0, 0, 540, 960);
-            g.setColor(new Color((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255), 255));
-            for (int i = 0; i < balls.size(); i++) { /*Loops through all balls to update physics */
-                g.fillOval((int)balls.get(i).x-5, (int)balls.get(i).y-5, 10, 10);
+
+            for (int i = 0; i < balls.size(); i++) { 
                 if (balls.get(i).x > 535) {
                     balls.get(i).xspeed *= -0.8;
                     balls.get(i).x = 535;
-                    balls.add(new ball(150, 150, (Math.random()-0.5)*5, (Math.random()-0.5)*5));
                 } else if (balls.get(i).x < 5) {
                     balls.get(i).xspeed *= -0.8;
                     balls.get(i).x = 5;
-                    balls.add(new ball(150, 150, (Math.random()-0.5)*5, (Math.random()-0.5)*5));
                 }
                 if (balls.get(i).y > 960) {
                     balls.get(i).yspeed *= -0.8;
                     balls.get(i).y = 960;
-                    balls.add(new ball(150, 150, (Math.random()-0.5)*5, (Math.random()-0.5)*5));
                 } else if (balls.get(i).y < 5) {
                     balls.get(i).yspeed *= -0.8;
                     balls.get(i).y = 5;
-                    balls.add(new ball(150, 150, (Math.random()-0.5)*5, (Math.random()-0.5)*5));
                 }
                 if (balls.get(i).xspeed > 0) {
                     balls.get(i).xspeed -= 0.005;
@@ -71,6 +70,8 @@ public class test {
                 balls.get(i).yspeed += 0.05;
                 balls.get(i).x += balls.get(i).xspeed;
                 balls.get(i).y += balls.get(i).yspeed;
+                g.setColor(new Color(balls.get(i).red, balls.get(i).green, balls.get(i).blue));
+                g.fillOval((int)balls.get(i).x-5, (int)balls.get(i).y-5, 10, 10);
             }
         }
     }
