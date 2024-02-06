ee90f22697677dd44ed13565e220115bdbf08ff6
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-02-06 10:10:54 -0600

    q

--- Cellular_Automata/RPC.java
+++ Cellular_Automata/RPC.java
@@ -2,35 +2,51 @@ package Cellular_Automata;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.*;
 
 class frame extends JFrame {
     @Override
     public void paint (Graphics g) {
         super.paint(g);
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, 50, 50);
+        
     }
 }
 
 public class RPC {
+
+    public static int width = 100;
+    public static int height = 100;
+
+    static Graphics graphics(Color[][] colors, Graphics oldGraphics) {
+        Graphics newGraphics = oldGraphics.create();
+        for (int i = 0; i < width; i++) {
+            for (int d = 0; d < width; d++) {
+                newGraphics.setColor(colors[i][d]);
+                newGraphics.drawRect(i, d, 1, 1);
+            }
+        }
+
+        return newGraphics;
+    }
+
     public static void main(String[] args) {
         frame frame = new frame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setUndecorated(true);
-        frame.setSize(540, 960);
+        frame.setSize(width, height);
         frame.setVisible(true);
         Graphics g = frame.getGraphics();
 
-        ArrayList<ArrayList<Color>> main = new ArrayList<ArrayList<Color>>();
+        Color[][] colors = new Color[width][height];
 
-        for (int i = 0; i < 100; i++) {
-            main.add(ArrayList<Color>(new Color(255, 255, 255)));
-            for (int d = 0; d < 100; d++) {
-                main.get(i).add(new Color(255, 255, 255));
+        for (int i = 0; i < width; i++) {
+            for (int d = 0; d < width; d++) {
+                colors[i][d] = new Color(255, 0, 0);
             }
         }
 
-        frame.paint(g);
+        while (true) {
+            g = graphics(colors, g);
+            frame.paint(g);
+        }
     }
 }
