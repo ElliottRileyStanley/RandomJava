42ca3cdd8b44be5cca00f16e34b3882bae608718
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-02-26 09:59:27 -0600

    shocked this works

--- maven testing/demo/src/main/java/com/example/Main.java
+++ maven testing/demo/src/main/java/com/example/Main.java
@@ -1,133 +1,38 @@
 package com.example;
- 
-import java.awt.AWTException;
-import java.awt.Dimension;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
+
+import java.awt.Graphics;
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
- 
+import java.awt.image.BufferedImage;
 import com.xuggle.mediatool.IMediaWriter;
 import com.xuggle.mediatool.ToolFactory;
-import com.xuggle.xuggler.ICodec;
- 
+
 public class Main {
- 
-    private static final double FRAME_RATE = 50;
- 
-    private static final int SECONDS_TO_RUN_FOR = 1;
- 
-    private static final String outputFilename = "C:\\Users\\Ellio\\OneDrive\\Desktop\\Coding\\Java\\RandomJava\\maven testing\\test.mp4";
- 
-    private static Dimension screenBounds;
- 
-    public static void main(String[] args) {
- 
-  // let's make a IMediaWriter to write the file.
- 
-  final IMediaWriter writer = ToolFactory.makeWriter(outputFilename);
- 
-  screenBounds = Toolkit.getDefaultToolkit().getScreenSize();
- 
-  // We tell it we're going to add one video stream, with id 0,
- 
-  // at position 0, and that it will have a fixed frame rate of FRAME_RATE.
- 
-  writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_MPEG4, 
- 
- screenBounds.width/2, screenBounds.height/2);
- 
-  long startTime = System.nanoTime();
- 
-  for (int index = 0; index < SECONDS_TO_RUN_FOR * FRAME_RATE; index++) {
- 
-// take the screen shot
- 
-BufferedImage screen = getDesktopScreenshot();
- 
-// convert to the right image type
- 
-BufferedImage bgrScreen = convertToType(screen, 
- 
- BufferedImage.TYPE_3BYTE_BGR);
- 
-// encode the image to stream #0
- 
-writer.encodeVideo(0, bgrScreen, System.nanoTime() - startTime, 
- 
- TimeUnit.NANOSECONDS);
- 
-// sleep for frame rate milliseconds
- 
-try {
- 
-    Thread.sleep((long) (1000 / FRAME_RATE));
- 
-} 
- 
-catch (InterruptedException e) {
- 
-    // ignore
- 
-}
- 
-  }
- 
-  // tell the writer to close and write the trailer if  needed
- 
-  writer.close();
- 
-    }
- 
-    public static BufferedImage convertToType(BufferedImage sourceImage, int targetType) {
- 
-  BufferedImage image;
- 
-  // if the source image is already the target type, return the source image
- 
-  if (sourceImage.getType() == targetType) {
- 
-image = sourceImage;
- 
-  }
- 
-  // otherwise create a new image of the target type and draw the new image
- 
-  else {
- 
-image = new BufferedImage(sourceImage.getWidth(), 
- 
-     sourceImage.getHeight(), targetType);
- 
-image.getGraphics().drawImage(sourceImage, 0, 0, null);
- 
-  }
- 
-  return image;
- 
-    }
- 
-    private static BufferedImage getDesktopScreenshot() {
- 
-  try {
- 
-Robot robot = new Robot();
- 
-Rectangle captureSize = new Rectangle(screenBounds);
- 
-return robot.createScreenCapture(captureSize);
- 
-  } 
- 
-  catch (AWTException e) {
- 
-e.printStackTrace();
- 
-return null;
- 
-  }
- 
-    }
- 
+	public static void main(String[] args) {
+		IMediaWriter writer = ToolFactory.makeWriter("maven testing\\test.mp4");
+		writer.addVideoStream(0, 0, 200, 200);
+		BufferedImage image = new BufferedImage(200, 200, BufferedImage.TYPE_3BYTE_BGR);
+		Graphics graphics = image.getGraphics();
+
+		int x = 10;
+		int y = 40;
+		int xspeed = 1;
+		int yspeed = 1;
+		for (int i = 0; i < 10000; i++) {
+			graphics.setColor(new Color(0, 0, 0));
+			graphics.fillRect(0, 0, 200, 200);
+			graphics.setColor(new Color(255, 255, 255));
+			graphics.fillRect(x-10, y-10, 20, 20);
+			x += xspeed;
+			y += yspeed;
+			if (x == 10 || x == 190) {
+				xspeed *= -1;
+			}
+			if (y == 10 || y == 190) {
+				yspeed *= -1;
+			}
+			writer.encodeVideo(0, image, i*5, TimeUnit.MILLISECONDS);
+		}
+		writer.close();
+	}
 }
