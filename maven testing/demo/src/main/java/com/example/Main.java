62c56960d53d712bc3de08a8888d05c33ec1c46e
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-02-28 16:46:11 -0600

    htnsuaouhehnshtaoueahnsuehnsu,.

--- maven testing/demo/src/main/java/com/example/Main.java
+++ maven testing/demo/src/main/java/com/example/Main.java
@@ -1,37 +1,72 @@
 package com.example;
 
-import java.awt.Graphics;
+import org.apache.commons.text.StringEscapeUtils;
+
 import java.awt.*;
 import java.util.concurrent.TimeUnit;
 import java.awt.image.BufferedImage;
 import com.xuggle.mediatool.IMediaWriter;
 import com.xuggle.mediatool.ToolFactory;
 
+import java.net.URL;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.json.JSONObject;
+
 public class Main {
+
+	static String question = "";
+	static String correct = "";
+	static String incorrect1 = "";
+	static String incorrect2 = "";
+	static String incorrect3 = "";
+	static String type = "";
+
+
+	public static void API() {
+		try {
+		URL url = new URL("https://opentdb.com/api.php?amount=1");
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		JSONObject response = new JSONObject(reader.readLine());
+		question = response.getJSONArray("results").getJSONObject(0).getString("question");
+		type = response.getJSONArray("results").getJSONObject(0).getString("type");
+		correct = response.getJSONArray("results").getJSONObject(0).getString("correct_answer");
+		incorrect1 = response.getJSONArray("results").getJSONObject(0).getJSONArray("incorrect_answers").getString(0);
+		if (type.equals("multiple")) {
+			incorrect2 = response.getJSONArray("results").getJSONObject(0).getJSONArray("incorrect_answers").getString(1);
+			incorrect3 = response.getJSONArray("results").getJSONObject(0).getJSONArray("incorrect_answers").getString(2);
+		}
+		} catch (IOException e) {}
+	}
+
 	public static void main(String[] args) {
 		IMediaWriter writer = ToolFactory.makeWriter("maven testing\\test.mp4");
-		writer.addVideoStream(0, 0, 200, 200);
-		BufferedImage image = new BufferedImage(200, 200, BufferedImage.TYPE_3BYTE_BGR);
+		writer.addVideoStream(0, 0, 1080, 1920);
+		BufferedImage image = new BufferedImage(1080, 1920, BufferedImage.TYPE_3BYTE_BGR);
 		Graphics graphics = image.getGraphics();
+		graphics.setFont(new Font("TimesRoman", Font.PLAIN, 50));
 
-		int x = 10;
-		int y = 40;
-		int xspeed = 1;
-		int yspeed = 1;
-		for (int i = 0; i < 10000; i++) {
-			graphics.setColor(new Color(0, 0, 0));
-			graphics.fillRect(0, 0, 200, 200);
+		API();
+
+		System.out.println(StringEscapeUtils.unescapeHtml4(type));
+		System.out.println(StringEscapeUtils.unescapeHtml4(question));
+		System.out.println(StringEscapeUtils.unescapeHtml4(correct));
+		System.out.println(StringEscapeUtils.unescapeHtml4(incorrect1));
+		System.out.println(StringEscapeUtils.unescapeHtml4(incorrect2));
+		System.out.println(StringEscapeUtils.unescapeHtml4(incorrect3));
+
+
+		for (int i = 0; i < 10; i++) {
 			graphics.setColor(new Color(255, 255, 255));
-			graphics.fillRect(x-10, y-10, 20, 20);
-			x += xspeed;
-			y += yspeed;
-			if (x == 10 || x == 190) {
-				xspeed *= -1;
-			}
-			if (y == 10 || y == 190) {
-				yspeed *= -1;
-			}
-			writer.encodeVideo(0, image, i*5, TimeUnit.MILLISECONDS);
+			graphics.fillRect(0, 0, 1080, 1920);
+			graphics.setColor(new Color(0, 0, 0));
+			graphics.drawString("AAAA", 100, 100);
+			writer.encodeVideo(0, image, i, TimeUnit.SECONDS);
 		}
 		writer.close();
 	}
