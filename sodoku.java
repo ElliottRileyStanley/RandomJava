3d0d34cbf171f7f005026e23ae91a1c8961a2a29
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-03-30 21:42:47 -0500

    a

--- sodoku.java
+++ sodoku.java
@@ -1,17 +1,32 @@
 import java.util.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.*;
 
 public class sodoku {
     static ArrayList<int[][]> boards = new ArrayList<>();
     static int[][] first_board = {
-        {8,6,9,1,3,5,7,4,2},
-        {5,2,1,4,7,8,9,3,6},
-        {3,4,7,9,2,6,0,8,0},
-        {6,7,8,3,4,1,5,2,9},
-        {1,3,4,5,9,2,8,6,7},
-        {9,5,2,8,6,7,3,1,4},
-        {0,0,3,2,8,4,6,5,1},
-        {4,1,6,7,5,3,2,9,8},
-        {2,8,5,6,1,9,4,7,3}};
+        {1,0,0,0,0,0,0,6,5},
+        {3,4,2,0,7,0,1,0,0},
+        {9,0,0,8,1,4,0,7,0},
+        {0,5,0,7,0,0,8,0,3},
+        {4,9,0,1,0,0,0,0,6},
+        {0,7,0,3,4,2,0,1,0},
+        {0,0,0,0,0,6,9,5,1},
+        {6,1,9,0,0,7,0,3,0},
+        {0,0,4,0,2,1,0,0,0}};
+
+        /* 
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0}
+        */
     
     public static void split(int x, int y) {
         for (int i = 1; i <= 9; i++) {
@@ -49,37 +64,63 @@ public class sodoku {
         }
         return true;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AWTException {
+        Point p = MouseInfo.getPointerInfo().getLocation();
+        Scanner scan = new Scanner(System.in);
+        Robot robot = new Robot(MouseInfo.getPointerInfo().getDevice());
         boards.add(first_board);
         int size;
+        int x_place = 0;
+        int y_place = 0;
+        int x = 0;
+        int y = 0;
         while (true) {
-            int x = (int) (Math.random() * 9);
-            int y = (int) (Math.random() * 9);
-            while (boards.get(0)[x][y] == 0) {
-                x = (int) (Math.random() * 9);
-                y = (int) (Math.random() * 9);
+            while (boards.get(0)[x_place][y_place] != 0) {
+                if (x_place == 8) {
+                    x_place = 0;
+                    y_place++;
+                } else {
+                    x_place++;
+                }
             }
-            System.out.println(boards.size());
+            x = x_place;
+            y = y_place;
             split(x, y);
             size = boards.size();
+            System.out.println(x + "   " + y + " - " + size);
             for (int i = size-1; i >= size-9; i--) {
                 if (!check(boards.get(i), x, y)) {
                     boards.remove(i);
                 }
             }
             
-            int min = 10;
-            for (int x_ind = 0; x_ind <= 8; x_ind++) {
-                for (int y_ind = 0; y_ind <= 8; y_ind++) {
-                    if (boards.get(0)[x_ind][y_ind] < min) {
-                        min = boards.get(0)[x_ind][y_ind];
-                    }
-                }
-            }
-            if (min != 0) {
+            if (x_place == 8 && y_place == 8) {
                 break;
             }
         }
-        System.out.println(Arrays.deepToString(boards.get(0)));
+        for (int[][] board : boards) {
+            System.out.println(Arrays.deepToString(board));
+        }
+        scan.nextLine();
+        int x1 = (int) MouseInfo.getPointerInfo().getLocation().getX();
+        int y1 = (int) MouseInfo.getPointerInfo().getLocation().getY();
+        scan.nextLine();
+        int x2 = (int) MouseInfo.getPointerInfo().getLocation().getX();
+        int y2 = (int) MouseInfo.getPointerInfo().getLocation().getY();
+        for (x = 0; x <= 8; x++) {
+            for (y = 0; y <= 8; y++) {
+                try {
+                    Thread.sleep(2000);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                robot.mouseMove(x1 + (x2-x1/8*x) - (int) MouseInfo.getPointerInfo().getLocation().getX(), y1 + (y2-y1/8*y) - (int) MouseInfo.getPointerInfo().getLocation().getY());
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                robot.keyPress(KeyEvent.VK_1);
+            }
+        }
     }
 }
