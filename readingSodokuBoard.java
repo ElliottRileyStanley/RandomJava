fb527646002bebdd637a19a0b48ba46df43d4d09
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-04-02 10:18:01 -0500

    uie

--- readingSodokuBoard.java
+++ readingSodokuBoard.java
@@ -13,13 +13,14 @@ public class readingSodokuBoard {
             {0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0}};
-        int x1 = 1025;
-        int y1 = 296;
-        int x2 = 1571;
-        int y2 = 842;
         Robot robot = new Robot(MouseInfo.getPointerInfo().getDevice());
+        int x1 = 991;
+        int y1 = 274;
+        int x2 = 1537;
+        int y2 = 819;
         for (int x = 0; x <= 8; x++) {
             for (int y = 0; y <= 8; y++) {
+                System.out.println(robot.getPixelColor(x1+(x2-x1)/8*x, y1+(y2-y1)/8*y));
                 if (robot.getPixelColor(x1+(x2-x1)/8*x, y1+(y2-y1)/8*y).getGreen() != 218 && robot.getPixelColor(x1+(x2-x1)/8*x, y1+(y2-y1)/8*y).getGreen() != 255) {
                     first_board[y][x] = 1;
                 }
