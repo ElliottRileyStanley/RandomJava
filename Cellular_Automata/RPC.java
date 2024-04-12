bfac5ee5be7eeef4908ee62a613c6631eb4d420c
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-04-12 06:54:35 -0500

    ixdb

--- Cellular_Automata/RPC.java
+++ Cellular_Automata/RPC.java
@@ -3,92 +3,52 @@ package Cellular_Automata;
 import javax.swing.*;
 import java.awt.*;
 
-class panel extends JPanel {
+public class RPC extends JPanel {
+
+    static int[][] prev_board = new int[100][100];
+    static int[][] new_board = new int[100][100];
+    
+
     @Override
-    public void paintComponent (Graphics g) {
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-    }
-}
-
-public class RPC {
-
-    public static int width = 500;
-    public static int height = 500;
-
-    static Graphics graphics(Color[][] colors, Graphics oldGraphics) {
-        Graphics newGraphics = oldGraphics.create();
-
-        for (int i = 0; i < width; i++) {
-            for (int d = 0; d < height; d++) {
-                int count = 0;
-                int red = 0;
-                int green = 0;
-                int blue = 0;
-                if (i > 0) {
-                    count++;
-                    red += colors[i-1][d].getRed();
-                    green += colors[i-1][d].getGreen();
-                    blue += colors[i-1][d].getBlue();
+        g.setColor(new Color(150, 150, 150));
+        
+        while (true) {
+            for (int x = 0; x < 100; x++) {
+                for (int y = 0; y < 100; y++) {
+                    int losses = 0;
+                    for (int x_dif = -1; x_dif <= 1; x_dif++) {
+                        for (int y_dif = -1; y_dif <= 1; y_dif++) {
+                            if ((x_dif == 0 && y_dif == 0) || (x+x_dif > 99 || x+x_dif < 1 || y+y_dif > 99 || y+y_dif < 1)) {continue;}
+                            if (prev_board[x][y] == 1 && prev_board[x+x_dif][y+y_dif] == 2) {losses++;}
+                            if (prev_board[x][y] == 2 && prev_board[x+x_dif][y+y_dif] == 3) {losses++;}
+                            if (prev_board[x][y] == 3 && prev_board[x+x_dif][y+y_dif] == 1) {losses++;}
+                        }
+                    }
+                    if (losses > 3) {prev_board[x][y] = (prev_board[x][y] % 3) + 1;}
+                    if (new_board[x][y] == 0) {g.setColor(new Color(255, 0, 0));}
+                    else if (new_board[x][y] == 1) {g.setColor(new Color(0, 255, 0));}
+                    else if (new_board[x][y] == 2) {g.setColor(new Color(0, 0, 255));}
+                    g.fillRect(x, y, 1, 1);
                 }
-                if (i < width-1) {
-                    count++;
-                    red += colors[i+1][d].getRed();
-                    green += colors[i+1][d].getGreen();
-                    blue += colors[i+1][d].getBlue();
-                }
-                if (d > 0) {
-                    count++;
-                    red += colors[i][d-1].getRed();
-                    green += colors[i][d-1].getGreen();
-                    blue += colors[i][d-1].getBlue();
-                }
-                if (d < height-1) {
-                    count++;
-                    red += colors[i][d+1].getRed();
-                    green += colors[i][d+1].getGreen();
-                    blue += colors[i][d+1].getBlue();
-                }
-                red /= count;
-                green /= count;
-                blue /= count;
-
-                colors [i][d] = new Color(red, green, blue);
             }
         }
 
-        for (int i = 0; i < width; i++) {
-            for (int d = 0; d < height; d++) {
-                newGraphics.setColor(colors[i][d]);
-                newGraphics.drawRect(i, d, 1, 1);
-            }
-        }
-
-        return newGraphics;
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setUndecorated(true);
-        frame.setSize(width, height);
-        frame.setVisible(true);
-        panel panel = new panel();
-        frame.add(panel);
-        Graphics g = frame.getGraphics();
-
-        Color[][] colors = new Color[width][height];
-
-        for (int i = 0; i < width; i++) {
-            for (int d = 0; d < height; d++) {
-                colors[i][d] = new Color((int)(Math.random() * 256), (int)(Math.random() * 256), (int)(Math.random() * 256));
+        for (int x = 0; x < 100; x++) {
+            for (int y = 0; y < 100; y++) {
+                prev_board[x][y] = (int) (Math.random()*3);
             }
         }
 
-        while (true) {
-            g = graphics(colors, g);
-            panel.paintComponent(g);
-            System.out.println(1);
-        }
+        JFrame frame = new JFrame("RPC");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setUndecorated(true);
+        frame.setSize(100, 100);
+        frame.add(new RPC());
+        frame.setVisible(true);
     }
 }
