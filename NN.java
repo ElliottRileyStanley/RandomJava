37c9d069c24b86ab0bf50aad0f617154de87767a
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-05-15 09:52:32 -0500

    aouoeiud

--- NN.java
+++ NN.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 class Connection {
     Double weight;
@@ -69,6 +70,8 @@ class Network {
     ArrayList<Node> inputs;
     ArrayList<Node> nodes = new ArrayList<Node>();
     ArrayList<Node> outputs = new ArrayList<Node>();
+    int prevSeed = 0;
+    Double prevRandomness = 0.0;
 
     public Network(ArrayList<Node> i, int h, int w, int o) {
         inputs = i;
@@ -121,29 +124,100 @@ class Network {
     }
 
     public void randomize(Double randomness) {
+        prevSeed = (int)(Math.random() * 1000000);
+        prevRandomness = randomness;
+        Random rand = new Random(prevSeed);
         for (Node node : nodes) {
             for (Connection connection : node.connections) {
-                connection.weight = connection.weight + (Math.random() - 0.5) * randomness * 2;
+                connection.weight = connection.weight + (rand.nextDouble() - 0.5) * randomness * 2;
             }
         }
         for (Node node : outputs) {
             for (Connection connection : node.connections) {
-                connection.weight = connection.weight + (Math.random() - 0.5) * randomness * 2;
+                connection.weight = connection.weight + (rand.nextDouble() - 0.5) * randomness * 2;
+            }
+        }
+    }
+
+    public void unrandomize() {
+        Random rand = new Random(prevSeed);
+        for (Node node : nodes) {
+            for (Connection connection : node.connections) {
+                connection.weight = connection.weight - (rand.nextDouble() - 0.5) * prevRandomness * 2;
+            }
+        }
+        for (Node node : outputs) {
+            for (Connection connection : node.connections) {
+                connection.weight = connection.weight - (rand.nextDouble() - 0.5) * prevRandomness * 2;
             }
         }
     }
 }
 
 public class NN {
-    public static void main(String[] args) {
-        ArrayList<Node> input = new ArrayList<Node>();
+    static ArrayList<Node> inputs = new ArrayList<Node>();
+    public static int fitness(Network net) {
         for (int i = 0; i < 9; i++) {
-            input.add(new Node());
+            inputs.get(i).value = 0.0;
         }
-        Network main = new Network(input, 2, 1, 9);
+        net.calc();
+        Double max = inputs.get(0).value;
+        int max_index = 0;
+        for (int i = 1; i < 9; i++) {
+            if (inputs.get(i).value > max) {
+                max_index = i;
+                max = inputs.get(i).value;
+            }
+        }
+        return (int)(Math.random() * 1000000000);
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            inputs.add(new Node());
+        }
+        Network net = new Network(inputs, 10, 10, 9);
+        int prevFit = fitness(net);
         while (true) {
-            Network alt = new Network(input, 2, 1, 9);
-
+            net.randomize(2.0);
+            int currFit = fitness(net);
+            if (currFit > prevFit) {
+                System.out.println("Better strategy found");
+                System.out.println(net);
+                prevFit = currFit;
+            } else {
+                System.out.println("Strategy is worse or equal");
+            }
         }
+
+        /* 
+        final int N_NETWORKS = 100;
+
+
+        ArrayList<Network> networks = new ArrayList<>();
+        for (int i = 0; i < N_NETWORKS; i++) {
+            networks.add(new Network(i, h, w, o));
+        }
+
+        ArrayList<Integer> scores = new ArrayList<>();
+        for (Network n: networks) {
+            int score = doSimulation(n);
+            scores.add(score);
+        }
+
+        int bestThreshold = 5;
+        int keepThreshold = 2;
+
+
+        for (int i = 0; i < N_NETWORKS; i++) {
+            if (scores.get(i) > bestThreshold) {
+                // do nothing
+            } else if (scores.get(i) > keepThreshold) {
+                networks.get(i).randomize(null);
+            } else {
+                networks.set(i, new Neto)
+            }
+        }
+        */
+
     }
 }
